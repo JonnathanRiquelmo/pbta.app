02dd23d14d65437fd83fe93c1f829c07574bc1af
JonnathanRiquelmo <jonnathan.riquelmo@gmail.com>
2025-12-09 16:50:07 -0300

    Revert "Merge branch 'fix/ci-cd-develop-branch'"
    This reverts commit 1c46d0abf8a07edb5e702f8a1865ee9ce23bfba2, reversing
    changes made to 48c40b7786513d37b02ec8f160a88fbc8536648c.

--- src/auth/types.ts
+++
@@ -1,9 +0,0 @@
-export type Role = 'master' | 'player'
-
-export type User = {
-  uid: string
-  email: string
-  displayName: string
-  role: Role
-  createdAt: string
-}
--- src/rolls/Route.tsx
+++
@@ -1,3 +0,0 @@
-export default function RollsRoute() {
-  return <div>Rolagens</div>
-}
